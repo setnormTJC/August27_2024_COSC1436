9d12c8ef5f84b391f08fc352887a6c6089875e4d
Work <Work@MSI>
2024-08-29 11:22:03 -0500

    Where we ended on August 29

---
+++ Fuel_Lab/Fuel_Lab.cpp
@@ -0,0 +1,29 @@
+// Fuel_Lab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	int a_number;  //this is an example of an "uninitialized" variable 
+	
+	std::cout << a_number << "\n";
+	//f(x) = x^2
+	//int is a "keyword" in C++ (Java, Python, ...) 
+	//keywords are displayed in a particular color in an IDE 
+
+	double CostOfAFullTank; //PascalCase
+	const double TAX_RATE = 0.0825; //SCREAMING_SNAKE_CASE
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
