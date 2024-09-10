1255f7a6065c6dde594749f3f45ff7428c4e1e68
Work <Work@MSI>
2024-09-10 12:12:15 -0500

    Where we ended on September 10 - resume around slide 25 of Topic/Chapter 2

---
+++ September10/September10.cpp
@@ -0,0 +1,66 @@
+
+
+#include <iostream>
+
+using namespace std; //
+
+int main()
+{
+
+    int a = 32; 
+
+    int b = 5; 
+
+    cout << a + b << endl; 
+    cout << a - b << endl; 
+    cout << a *b << endl; 
+    cout << a << "/" << b << " = " << a / b << "\n";
+    cout << a << "%" << b << " = " << a % b << "\n"; //mod operator -> % 
+
+    //string firstName;
+
+    //int number1 + number2 = 43 + 8676; //= -> the "assignment operator" 
+
+    
+    //string namespace;
+
+    //cout << sizeof(a) << "\t" << sizeof(int) << "\n";
+    //cout << pow(3, 2) << "\n"; //2 ^ 3 = 2(2)(2) = 4(2) = 8
+    //2
+    //unsigned int someNumber = pow(2, 32) - 1; //(2 ^ 32) - 1 
+    //
+    //std::locale thisLocale(""); 
+    //cout.imbue(thisLocale);
+
+    //cout << std::hex; 
+    //cout << someNumber << "\n";
+    //cout << (someNumber >> 8) << "\n"; //rightshift 8 bits
+
+    //print("asdfsadf")
+    //int someBigNumber = 9999999999999999; 
+    //int maxValueForInteger = INT_MAX;
+
+    //cout << "max int value is: " << maxValueForInteger + 1<< "\n";
+
+    float someFloat = 2.5; 
+    double eulersNumber = 2.718; 
+
+    //cout << someFloat << "\n";
+    char someLetter = 'a';//when you set a variable equal to something 
+                            //that is called "initialization" 
+    //bool 
+    //string //this is not a "primitive" data type 
+    //string secondWord; //variable "declaration" 
+
+    //cout << someLetter * 2 << endl; 
+
+
+    //string someWord = "alpha"; 
+
+    //cout << "someNumber is: "<< someNumber * 2<< "\n";
+    //cout << "some word is: " << someWord << "\n";
+
+    //cout << //c means "character" put characters out to the "terminal/console"
+    //std::cout << "Hello World!\n";
+}
+
